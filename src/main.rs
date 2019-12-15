17db531ebd942ef3f9f8159ea53b244e9ac2251c
Esa-Matti Suuronen <esa-matti@suuronen.org>
2019-12-15 14:27:12 +0200

    more oop

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,14 @@ use std::sync::mpsc;
 
 use std::thread;
 
+type Channel = std::sync::mpsc::Sender<Message>;
+
 struct MultipChild<'a> {
     name: &'a str,
     pid: Pid,
     kill_sent: bool,
     exit_status: Option<ExitStatus>,
+    tx: &'a Channel,
 }
 
 impl fmt::Display for MultipChild<'_> {
@@ -24,10 +27,8 @@ impl fmt::Display for MultipChild<'_> {
     }
 }
 
-type Channel = std::sync::mpsc::Sender<Message>;
-
 impl MultipChild<'_> {
-    fn spawn<'a>(name: &'a str, command: &str, tx: &Channel) -> MultipChild<'a> {
+    fn spawn<'a>(name: &'a str, command: &str, tx: &'a Channel) -> MultipChild<'a> {
         let mut cmd = Command::new("/bin/sh")
             .arg("-c")
             .arg(command)
@@ -35,35 +36,29 @@ impl MultipChild<'_> {
             .stderr(Stdio::piped())
             .spawn()
             .ok()
-            .expect("failed to spwan");
+            .expect("failed to spawn command");
 
-        let stdout = cmd.stdout.take().expect("lol");
-        let stderr = cmd.stderr.take().expect("lol");
-
-        let tx1 = mpsc::Sender::clone(&tx);
-        let tx2 = mpsc::Sender::clone(&tx);
-        capture_output_as_lines(name, stdout, tx1);
-        capture_output_as_lines(name, stderr, tx2);
+        let stdout = cmd.stdout.take().expect("failed to take stdout");
+        let stderr = cmd.stderr.take().expect("failed to take stderr");
 
         let pid = cmd.id() as i32;
         println!("Started {} as {}", name, pid);
 
-        let name2 = name.to_string();
-        let tx3 = mpsc::Sender::clone(&tx);
-        thread::spawn(move || {
-            let res = cmd.wait().expect("exit failed");
-            println!("{} exited {}", name2, res);
-            tx3.send(Message::Exit(name2, res)).unwrap();
-        });
-
         let pid = Pid::from_raw(pid);
 
-        MultipChild {
+        let child = MultipChild {
             name,
+            tx,
             pid,
             exit_status: None,
             kill_sent: false,
-        }
+        };
+
+        child.monitor_ouput(stdout);
+        child.monitor_ouput(stderr);
+        child.monitor_for_exit(cmd);
+
+        child
     }
 
     fn kill(&mut self) {
@@ -76,6 +71,28 @@ impl MultipChild<'_> {
         if kill(self.pid, SIGTERM).is_err() {
             println!("kill failed for {}", self.name);
         }
+    }
+
+    fn monitor_for_exit(&self, mut cmd: std::process::Child) {
+        let tx = mpsc::Sender::clone(self.tx);
+        let name = self.name.to_string();
+        thread::spawn(move || {
+            let res = cmd.wait().expect("exit failed");
+            println!("{} exited {}", name, res);
+            tx.send(Message::Exit(name, res)).unwrap();
+        });
+    }
+
+    fn monitor_ouput(&self, stream: impl Read + Send + 'static) -> std::thread::JoinHandle<()> {
+        let name = self.name.to_string();
+        let tx = mpsc::Sender::clone(self.tx);
+        thread::spawn(move || {
+            let buf = BufReader::new(stream);
+            for line in buf.lines() {
+                let name = name.to_string();
+                tx.send(Message::Line(Line { name, line })).unwrap();
+            }
+        })
     }
 
     fn is_alive(&self) -> bool {
@@ -97,21 +114,6 @@ impl fmt::Display for Line {
 enum Message {
     Line(Line),
     Exit(String, std::process::ExitStatus),
-}
-
-fn capture_output_as_lines(
-    name: &str,
-    stream: impl Read + Send + 'static,
-    tx: Channel,
-) -> std::thread::JoinHandle<()> {
-    let name = name.to_string();
-    thread::spawn(move || {
-        let buf = BufReader::new(stream);
-        for line in buf.lines() {
-            let name = name.to_string();
-            tx.send(Message::Line(Line { name, line })).unwrap();
-        }
-    })
 }
 
 fn command_with_name(s: &String) -> (&str, &str) {
