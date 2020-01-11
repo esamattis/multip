3536ee91947448e58a202b5accbdd5e637b3547c
Esa-Matti Suuronen <esa-matti@suuronen.org>
2020-01-11 23:11:30 +0200

    Add integration tests

---
+++ tests/test_cli.rs
@@ -0,0 +1,91 @@
+use std::io::{BufRead, BufReader};
+use std::process::{id, ChildStdout, Command, Stdio};
+
+fn run_multip(args: Vec<&str>) -> Command {
+    let mut cmd = Command::new("target/debug/multip");
+
+    cmd.stdout(Stdio::piped());
+    cmd.stderr(Stdio::piped());
+    cmd.args(args);
+
+    cmd
+}
+
+fn get_lines(out: Option<ChildStdout>) -> Vec<String> {
+    let out = out.expect("get stdout/stderr");
+    let mut lines: Vec<String> = Vec::new();
+    let buf = BufReader::new(out);
+    for line in buf.lines() {
+        let line = line.unwrap_or("".to_string());
+        lines.push(line);
+    }
+    lines
+}
+
+fn assert_has_line(lines: &Vec<String>, needle_line: &str) {
+    let found = lines.iter().find(|&line| line.trim() == needle_line);
+
+    if found.is_some() {
+        return;
+    }
+
+    for line in lines {
+        eprintln!("LINE> {}", line);
+    }
+
+    panic!("Failed to find line: {}", needle_line);
+}
+
+#[test]
+fn run_single_command() {
+    let mut cmd = run_multip(vec!["foo: sh -c 'echo hello'"]).spawn().unwrap();
+
+    let lines = get_lines(cmd.stdout.take());
+
+    assert_has_line(&lines, "[foo] hello");
+
+    cmd.wait().unwrap();
+}
+
+#[test]
+fn run_multiple_commands() {
+    let mut cmd = run_multip(vec![
+        "foo: sh -c 'echo hello foo && sleep 0.1'",
+        "bar: sh -c 'echo hello bar && sleep 0.1'",
+    ])
+    .spawn()
+    .unwrap();
+
+    let lines = get_lines(cmd.stdout.take());
+
+    assert_has_line(&lines, "[foo] hello foo");
+    assert_has_line(&lines, "[bar] hello bar");
+
+    cmd.wait().unwrap();
+}
+
+#[test]
+fn uses_exit_code_of_first_dead_child() {
+    let mut cmd = run_multip(vec![
+        "foo: sh -c 'sleep 0.1 && exit 11'",
+        "bar: sh -c 'sleep 0.2 && exit 22'",
+    ])
+    .spawn()
+    .unwrap();
+
+    let status_code = cmd.wait().unwrap().code().unwrap();
+    assert_eq!(status_code, 11);
+}
+
+#[test]
+fn uses_exit_code_of_first_dead_child_with_zore() {
+    let mut cmd = run_multip(vec![
+        "foo: sh -c 'sleep 0.1 && exit 0'",
+        "bar: sh -c 'sleep 0.2 && exit 22'",
+    ])
+    .spawn()
+    .unwrap();
+
+    let status_code = cmd.wait().unwrap().code().unwrap();
+    assert_eq!(status_code, 0);
+}
