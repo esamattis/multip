5fb2c76a3b8230f739773627348cf6afd6079a62
Esa-Matti Suuronen <esa-matti@suuronen.org>
2020-01-11 23:03:01 +0200

    Add debug macro

--- src/log.rs
+++ src/log.rs
@@ -7,3 +7,18 @@ macro_rules! log {
         println!($($arg)*);
     }
 }
+
+#[macro_export]
+macro_rules! debug {
+    () => {
+        if std::env::var("MULTIP_DEBUG").is_ok() {
+            println!();
+        }
+    };
+    ($($arg:tt)+) => {
+        if std::env::var("MULTIP_DEBUG").is_ok() {
+            print!("<DEBUG> ");
+            println!($($arg)*);
+        }
+    }
+}
